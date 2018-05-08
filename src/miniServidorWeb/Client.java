b977f4221d8f8899a6b501b556b8b86948c7025e
José Ricardo <zevatron@zevatron.com.br>
2018-05-08 11:05:29 -0300

    Pesquisar , ler e exibir arquivos...

--- src/miniServidorWeb/Client.java
+++ src/miniServidorWeb/Client.java
@@ -1,23 +1,53 @@
 package miniServidorWeb;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String host = args[0].split(":")[0];
-		int port = Integer.parseInt( args[0].split(":")[1].split("/")[0] );
-		String file = args[0].split(":")[1].split("/")[1];
+		String host, file;
+		int port;
 		
+		if(args == null || args.length==0 ){
+			System.out.println("Digite o endereço e tente novamente.");
+			return;
+		}
+		host = args[0].split(":")[0];
 		try {
-			
-			Socket socket = new Socket(host, port);
+			port = Integer.parseInt(args[0].split(":")[1].split("/")[0]);			
+		} catch (Exception e) {
+			System.out.println("Redirecionando para a porta 6500...");
+			port = 6500;
+		}
+		try{
+			file = args[0].split(":")[1].split("/")[1] ;
+		}catch(Exception e ){
+			file = "index.html";
+		}
+
+		try {
+
+			Socket socket = new Socket(host.split("/")[0], port);
 			System.out.println("Conectado no servidor...");
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-			out.writeUTF("Olá mundo! " + file);
+			DataOutputStream request = new DataOutputStream(socket.getOutputStream());
+			DataInputStream response = new DataInputStream(new DataInputStream(socket.getInputStream()));
+
+			request.writeUTF(file);
+			String status ;
+			status = response.readUTF();
+			if(status.equals("200")){				
+				do{
+					status = response.readUTF();
+					if(!status.equals("\\EOF"))
+						System.out.println(status);
+				}while(!status.equals("404") && !status.equals("\\EOF"));
+			}else
+				System.out.println(status + " Not Found\nPágina não encontrada!");
+			
 			try {
 				Thread.sleep(10000);
 				socket.close();
@@ -26,16 +56,13 @@ public class Client {
 				e.printStackTrace();
 			}
 			System.out.println("desconectado.");
-//			DataInputStream in = new DataInputStream(socket.getInputStream());
-			
-//			do{
-//				out.writeUTF(JOptionPane.showInputDialog("Mensagem: "+Thread.currentThread().getName()));
-//			} while (!socket.isClosed());
-			
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
+			
+			System.out.println("Servidor não encontrado!\n"
+					+ "Verifique se a URL "+ host +":"+port+ " está correta.\n"
+					+ "Tente novamente...");
+		}
 
 	}
 
